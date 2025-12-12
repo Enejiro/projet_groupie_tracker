61be25f9cf68b6be496469342d850561b914d3e4
fyanael <yanael.fillon@ynov.com>
2025-12-12 13:56:42 +0100

    feat/API SearchArtist

---
+++ API.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Artist struct {
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Members      []string `json:"members"`
+	Creation     int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func SearchArtist(id int) (*Artist, error) {
+	url := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%d", id)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var artist Artist
+	err = json.Unmarshal(body, &artist)
+	if err != nil {
+		return nil, err
+	}
+
+	return &artist, nil
+}
--- server.go
+++ server.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func StartServer() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "templates/index.html")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/index.html")
+	})
 
-    log.Println("Serveur en écoute sur http://localhost:8888")
-    if err := http.ListenAndServe(":8888", nil); err != nil {
-        log.Fatalf("Erreur au lancement du serveur: %v", err)
-    }
+	log.Println("Serveur en écoute sur http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Erreur au lancement du serveur: %v", err)
+	}
 }
-
