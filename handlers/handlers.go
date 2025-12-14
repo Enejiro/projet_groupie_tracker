2ff2b3df187ffb7db6e631ebd4b6403fec2ed0ab
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2025-12-14 16:05:35 +0100

    add new handlers with a test artist until the API are available

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,23 +1,84 @@
 package handlers
 
 import (
+    "fmt"
     "html/template"
     "net/http"
     "projet_groupie_tracker/models"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        ErrorHandler(w, "Page non trouvée", 404)
+        return
+    }
+
     tmpl, err := template.ParseFiles("templates/home.html")
     if err != nil {
         http.Error(w, "Erreur template", 500)
         return
     }
 
-    artist := models.Artist{
-        Name:         "Queen",
-        CreationDate: 1970,
-        Members:      4,
+    tmpl.Execute(w, nil)
+}
+
+
+func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
+    artists := GetTestArtists()
+
+    tmpl, err := template.ParseFiles("templates/artists.html")
+    if err != nil {
+        ErrorHandler(w, "Erreur template", 500)
+        fmt.Println("Erreur template:", err)
+        return 
     }
 
+    err = tmpl.Execute(w,artists)
+    if err != nil {
+        fmt.Println("Erreur exécution:", err)
+    }
+}
+
+func ArtistDetailHandler(w http.ResponseWriter, r *http.Request) {
+    idStr := r.URL.Query().Get("id")
+    if idStr == "" {
+        ErrorHandler(w, "ID manquant", 400)
+        return 
+    }
+
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        ErrorHandler(w, "ID invalide", 400)
+        return 
+    }
+
+    artist := GetTestArtistByID(id)
+    if artist == nil {
+        ErrorHandler(w, "Artiste non trouvé", 404)
+        return 
+    }
+
+    tmpl, err := template.ParseFiles("templates/artist-detail.html")
+    if err != nil {
+        ErrorHandler(w, "Erreur template", 500)
+        return 
+    }
+    
     tmpl.Execute(w, artist)
+}
+
+func ErrorHandler(w http.ResponseWriter, status int, message string) {
+    w.WriteHeader(status)
+    tmpl, err := template.ParseFiles("templates/error.html")
+    if err != nil {
+        http.Error(w, message, status)
+        return 
+    }
+
+    data := map[string]interface{}{
+        "Status": status, 
+        "Message": message, 
+    }
+
+    tmpl.Execute(w,data)
 }
