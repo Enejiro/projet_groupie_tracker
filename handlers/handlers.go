c20b0d2ae37e66436ee02365c5d86f1090a737c6
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2025-12-15 14:24:12 +0100

    update feat/handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,8 +4,7 @@ import (
     "fmt"
     "html/template"
     "net/http"
-    "projet_groupie_tracker/models"
-    "strconv"
+    "strconv"  > import string conversion pour convertir entre strings et nombres 
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,25 +15,24 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
     tmpl, err := template.ParseFiles("templates/home.html")
     if err != nil {
-        http.Error(w, "Erreur template", 500)
+        ErrorHandler(w, "Erreur template", 500)
         return
     }
 
     tmpl.Execute(w, nil)
 }
 
-
 func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
     artists := GetTestArtists()
 
     tmpl, err := template.ParseFiles("templates/artists.html")
     if err != nil {
-        ErrorHandler(w, "Erreur template", 500)
+        ErrorHandler(w, 500, "Erreur template")
         fmt.Println("Erreur template:", err)
-        return 
+        return
     }
 
-    err = tmpl.Execute(w,artists)
+    err = tmpl.Execute(w, artists)
     if err != nil {
         fmt.Println("Erreur exécution:", err)
     }
@@ -44,42 +42,48 @@ func ArtistDetailHandler(w http.ResponseWriter, r *http.Request) {
     idStr := r.URL.Query().Get("id")
     if idStr == "" {
         ErrorHandler(w, "ID manquant", 400)
-        return 
+        return
     }
 
     id, err := strconv.Atoi(idStr)
     if err != nil {
         ErrorHandler(w, "ID invalide", 400)
-        return 
+        return
     }
 
     artist := GetTestArtistByID(id)
     if artist == nil {
         ErrorHandler(w, "Artiste non trouvé", 404)
-        return 
+        return
     }
 
     tmpl, err := template.ParseFiles("templates/artist-detail.html")
     if err != nil {
         ErrorHandler(w, "Erreur template", 500)
-        return 
+        return
     }
-    
+
     tmpl.Execute(w, artist)
 }
+
 
 func ErrorHandler(w http.ResponseWriter, status int, message string) {
     w.WriteHeader(status)
     tmpl, err := template.ParseFiles("templates/error.html")
     if err != nil {
         http.Error(w, message, status)
-        return 
+        return
     }
 
     data := map[string]interface{}{
-        "Status": status, 
-        "Message": message, 
+        "Status":  status,
+        "Message": message,
     }
 
-    tmpl.Execute(w,data)
+    tmpl.Execute(w, data)
 }
+
+
+
+
+
