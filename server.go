0efa6d19a1ad1520a77eb434f214ba33a93c7f66
fyanael <yanael.fillon@ynov.com>
2025-12-15 13:50:12 +0100

    feat/css

--- server.go
+++ server.go
@@ -10,6 +10,9 @@ func StartServer() {
 		http.ServeFile(w, r, "templates/index.html")
 	})
 
+	css := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
+
 	log.Println("Serveur en écoute sur https://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Erreur au lancement du serveur: %v", err)
