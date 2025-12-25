a3ba867088dbad92e30dc0a0567b5cf8789d8fd8
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2025-12-25 12:25:53 +0100

    fix : backend handlers structure complete, just missing the API

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -23,7 +23,19 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
-	artists := GetTestArtists()               // à remplacer par l'API //
+
+     tmpl, err := template.ParseFiles("templates/artists.html")
+    if err != nil {
+        ErrorHandler(w, 500, "Erreur de chargement de la page")
+        fmt.Println("Erreur template artists:", err)
+        return
+    }
+
+    var emptyArtists []interface{}
+    tmpl.Execute(w, emptyArtists)
+
+	/*
+    artists := GetTestArtists()               // à remplacer par l'API //
 
 	tmpl, err := template.ParseFiles("templates/artists.html")
 	if err != nil {
@@ -36,9 +48,40 @@ func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Erreur exécution:", err)
 	}
+    */
 }
 
+/*
 func ArtistDetailHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("templates/artists.html")
+    if err != nil {
+        ErrorHandler(w, 500, "Erreur de chargement de la page")
+        fmt.Println("Erreur template artists:", err)
+        return
+    }
+
+    var emptyArtists []interface{}
+    tmpl.Execute(w, emptyArtists)
+}
+*/
+
+func ArtistDetailHandler(w http.ResponseWriter, r *http.Request) {
+    idStr := r.URL.Query().Get("id")
+    if idStr == "" {
+        ErrorHandler(w, 400, "L'ID de l'artiste est manquant")
+        return
+    }
+
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        ErrorHandler(w, 400, "L'ID doit être un nombre valide")
+        return
+    }
+
+    w.Write([]byte(fmt.Sprintf("<h1>Page de détails pour l'artiste ID %d</h1><p>En attente de l'API...</p><a href='/artists'>Retour</a>", id)))
+}
+
+    /*
 	idStr := r.URL.Query().Get("id")
 	if idStr == "" {
 		ErrorHandler(w, 400, "ID manquant")
@@ -64,7 +107,7 @@ func ArtistDetailHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, artist)
-}
+    */
 
 func ErrorHandler(w http.ResponseWriter, status int, message string) {
 	w.WriteHeader(status)
