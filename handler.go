75db34fe969265b85c974ce1e9b9d841329e613b
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2026-02-09 23:13:47 +0100

    add error in handler.go

--- handler.go
+++ handler.go
@@ -6,6 +6,26 @@ import (
 	"net/http"
 	"strconv"
 )
+type ErrorData struct {
+    Status  int
+    Message string
+}
+func showError(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
+
+	tmpl, err := template.ParseFiles("templates/error.html")
+	if err != nil {
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+		return
+	}
+
+	data := ErrorData{
+		Status:  status,
+		Message: message,
+	}
+
+	tmpl.Execute(w, data)
+}
 
 func _Handler() {
 
@@ -15,25 +35,67 @@ func _Handler() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			showError(w, 404, "")
+			return
+		}
+
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		_ = tmpl.Execute(w, artists)
+		err = tmpl.Execute(w, artists)
+		if err != nil {
+			showError(w, 500, "Erreur lors de l'affichage de la page")
+		}
 	})
 
 	http.HandleFunc("/map", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/map.html"))
-		_ = tmpl.Execute(w, artists)
+		err = tmpl.Execute(w, artists)
+		if err != nil {
+			showError(w, 500, "Erreur lors de l'affichage de la carte")
+		}
 	})
 
 	http.HandleFunc("/artistes", func(w http.ResponseWriter, r *http.Request) {
 		idStr := r.URL.Query().Get("id")
-		id, _ := strconv.Atoi(idStr)
-		artist, _ := getOneArtist(id)
+		if idStr == "" {
+			showError(w, 400, "ID d'artiste manquant")
+			return
+		}
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			showError(w, 400, "ID d'artiste invalide")
+			return
+		}
+
+		if id <= 0 {
+			showError(w, 400, "ID doit être supérieur à 0")
+			return
+		}
+
+		if id > 52 {
+			showError(w, 404, "Artiste introuvable")
+			return
+		}
+
+		artist, err := getOneArtist(id)
+		if err != nil {
+			showError(w, 500, "Erreur lors de la récupération de l'artiste")
+			return
+		}
+
 		tmpl := template.Must(template.ParseFiles("templates/artistes.html"))
-		_ = tmpl.Execute(w, artist)
+		err = tmpl.Execute(w, artist)
+		if err != nil {
+			showError(w, 500, "Erreur lors de l'affichage de la page artiste")
+		}
 	})
 
 	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/contact.html"))
-		_ = tmpl.Execute(w, artists)
+		err = tmpl.Execute(w, artists)
+		if err != nil {
+			showError(w, 500, "Erreur lors de l'affichage de la page de contact")
+		}
 	})
 }
